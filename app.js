18ddd46f550191b62a4487092cbbcb9584a42462
Todd Malone <toddomalone@gmail.com>
2021-02-27 16:34:21 -0600

    removed scaffolding comments, added other comments for clarity

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const { rejects } = require("assert");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-//TODO: Should we only allow one manager?
 const questions =[
     {
         type:"list",
@@ -133,7 +129,7 @@ const ensureOutputDir = new Promise(function(resolve,reject){
     }
 });
 
-//DEBUG; verify ensureOutputDir functions as expected
+//DEBUG: verify ensureOutputDir functions as expected
 function dirTest(){
     ensureOutputDir
     .then(response=>{
@@ -148,26 +144,6 @@ function dirTest(){
 };
 
 
+// dirTest();//DEBUG
 init();
-// dirTest();
 
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
